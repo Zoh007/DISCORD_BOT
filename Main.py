e0078b5aa1e0ea315f0f3e6c5b43e5b4ce98b23b
Zohair Hussain <iqbalzohair03@gmail.com>
2024-01-19 12:34:56 -0500

    UPDATED

--- Main.py
+++ Main.py
@@ -10,23 +10,24 @@ print("Token is: " + Token + "\n")
 async def on_ready():
   print("online", bot.user.name)
 
-
+# Only uncomment the following code if know your channel id's
 @bot.command()
 async def hi(ctx):
-  if ctx.channel.id == 1087788515623977000:
+  channel_id = input("What is your channel id? ")
+  if ctx.channel.id == channel_id:
     author_name = ctx.author.name
     await ctx.channel.send(
       'This is the designated channel for welcoming new members!')
     await ctx.send("Hi " + author_name)
 
 
-@bot.command()
-async def testmessage(ctx, user_mention, *, message):
-  if ctx.channel.id == 1156608903832940674:
-    user_send = ctx.message.mentions[0]
-    await user_send.send("Message: " + message + " from " + ctx.author.name)
-  else:
-    None
+# @bot.command()
+# async def testmessage(ctx, user_mention, *, message):
+#   if ctx.channel.id == 1156608903832940674:
+#     user_send = ctx.message.mentions[0]
+#     await user_send.send("Message: " + message + " from " + ctx.author.name)
+#   else:
+#     None
 
 
 bot.run(Token)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # DISCORD_BOT
-I am making a discord bot
+I have made a discord bot.
+
+FIRST of all, if you guys don't have the discord module downloaded, please use the following command to do it:
+
+    pip install discord.py
+
+After that, if the code shows the following error,
+
+No module named 'discord', line 1, in <module>
+    import discord
+ModuleNotFoundError: No module named 'discord',
+
+Go to the interpretor and locate where you downloaded the discord.py file.
+
+After that, copy the path and change the interpretor and you are all good to go.
+
+
+
